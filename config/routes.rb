2c1d2187d6fa9ee4b43d6a0b0be57bdc5ddf6ade
Brian Wangila <bwanjalah@gmail.com>
2022-09-28 01:50:02 +0300

    donedone

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
-  get '/cheeses/:id', to: 'cheeses#show'
+  # get '/cheeses/:id', to: 'cheeses#show'
+  get '/cheeses/:id', to: 'cheeses#show_method'
+
 end
